415f86a3794941082a94e3449a70ae74e13c2733
LucaCampion <luca212005@gmail.com>
2023-11-08 16:53:44 +0000

    Modifica vecchio lavoro

--- www/5H_Campion/PaginaRiservata/PaginaRiservata2.0/index.php
+++ www/5H_Campion/PaginaRiservata/PaginaRiservata2.0/index.php
@@ -6,30 +6,38 @@
     <title>Log in by Luca Campion</title>
 </head>
 <body>
-<?php
-    if(isset($_POST["Invio"])) {
-        if(isset($_POST["Lucio"])) {
-            $utente=$_POST["Lucio"]; 
-        }       
-          if(isset($_POST["WordPass"])) {
-                $password=$_POST["WordPass"]; 
-            }
-    }
-    else {
-        echo "Attenzione, nome o password errati!";
-    }
-    if($utente!="Lucio"||$password!="WordPass") {
-        echo "Attenzione, nome o password errati!";
-    }
-    else {
-        echo "<h3>Benvenuto $utente sei entrato nell'area riservata</h3>";
-    }
-    ?>
     <h2>Credenziali richieste per l'accesso alla pagina riservata<br></h2>
-    <form name="frmLogin" action="" method="POST">
-         Username: <input type="text" name="Lucio" required><br>
+<?php
+$utente = "";
+$password = "";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {    // Controlla se il form è stato inviato tramite POST
+    if (isset($_POST["Luca"])) {    //Controlla se il campo è compilato ed inviato
+        $utente = $_POST["Luca"];   //Assegna il valore alla variabile
+    }
+    if (isset($_POST["WordPass"])) {
+        $password = $_POST["WordPass"];
+    }
+
+    if ($utente == "Luca" && $password == "WordPass") {     //Controlla se le credenziali inserite sono corrette
+        echo "<h3>Benvenuto $utente sei entrato nell'area riservata!</h3>";     //Stampa messaggio di benvenuto
+    } else {
+        echo "Attenzione, nome o password errati!";     //Stampa messaggio di errore
+        //Ripropone il form
+        echo '<form name="frmLogin" action="" method="POST">    
+         Username: <input type="text" name="Luca" required><br>
          Password: <input type="password" name="WordPass" required>
-         <input type="submit" value="Invio">        
-    </form>
-</body> 
+        <input type="submit" value="Invio" >
+</form>';
+    }
+} 
+else {  //Stampa del form
+    echo '<form name="frmLogin" action="" method="POST">
+    Username: <input type="text" name="Luca" required><br>
+    Password: <input type="password" name="WordPass" required>
+    <input type="submit" value="Invio" >
+</form>';
+}
+?>
+</body>
 </html>
